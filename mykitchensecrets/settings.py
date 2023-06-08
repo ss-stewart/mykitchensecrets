631dde6e81e06f6a46cf4ac712ea790ad4f24e8d
ss-stewart <ss_stewart92@yahoo.com>
2023-06-08 11:15:27 -0400

    deploy debug test

--- mykitchensecrets/settings.py
+++ mykitchensecrets/settings.py
@@ -38,7 +38,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
+# DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
